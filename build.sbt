a228dff3cada6687ba6285fa623c6d016df5bee8
CHIKKAR <r.chikka@elsevier.com>
2017-07-17 17:37:01 +0100

    ravi test 1

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "DB_HW"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+        
---
+++ src/main/scala/hw.scala
@@ -0,0 +1,9 @@
+/**
+  * Created by CHIKKAR on 17/07/2017.
+  */
+object hw {
+  def main(args: Array[String]): Unit = {
+    println("hello world!")
+  }
+
+}
